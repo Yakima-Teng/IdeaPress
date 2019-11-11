3192dd645baf8bf57e0d11c3965b4379364d6602
Yakima <cleveryun@163.com>
2019-11-11 23:29:52 +0800

    add Powered by text

--- src/components/frontend/Footer.jsx
+++ src/components/frontend/Footer.jsx
@@ -3,12 +3,13 @@ import { seo } from '../../../site.config'
 const Footer = () => (
     <div>
         <span>&copy; {new Date().getFullYear()}</span>
-        <span className="siteName">峰间的云</span>
+        <span className="siteName">{seo.siteMainTitle}</span>
         {
             !!seo.beian && (
                 <a href="http://www.miitbeian.gov.cn/" className="beian" target="_blank" rel="nofollow noopener noreferrer">{seo.beian}</a>
             )
         }
+        <span className="poweredBy">Powered by <a href="http://www.orzzone.com" target="_blank" rel="noopener noreferrer">IdeaPress</a></span>
         <script src="//cdn.bootcss.com/jquery/1.12.4/jquery.min.js" />
         <script src="//cdn.bootcss.com/layer/2.3/layer.js" />
         <script src="//cdn.bootcss.com/twitter-bootstrap/3.4.1/js/bootstrap.min.js" />
@@ -26,6 +27,10 @@ const Footer = () => (
             .beian {
                 text-decoration: underline;
                 color: inherit;
+            }
+            .poweredBy {
+                display: inline;
+                margin-left: 6px;
             }
         `}</style>
     </div>
