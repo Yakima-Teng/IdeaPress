4d4cb2305a5604390b00826420e34d97d8e485f3
Yakima Teng <cleveryun@163.com>
2020-03-13 17:12:01 +0800

    do not show unapproved comment

--- src/servers/v2/getRandomComments.js
+++ src/servers/v2/getRandomComments.js
@@ -4,7 +4,7 @@ export const getRandomComments = async () => {
     const randomComments = (await promiseQuery(
         'SELECT c.*, p.post_title, p.post_name ' +
         'FROM wp_comments c, wp_posts p ' +
-        'WHERE p.post_status = "publish" AND p.post_type = "post" AND c.comment_post_ID = p.ID ' +
+        'WHERE p.post_status = "publish" AND p.post_type = "post" AND c.comment_post_ID = p.ID AND c.comment_approved = "1" ' +
         'ORDER BY RAND() LIMIT 3;'
     )).map((item) => ({ ...item }))
 
